81aacceaa68ac237e096d588e33123b7ee0f5fd4
lnsx <zhang13652103006@163.com>
2016-12-26 10:29:06 +0800

    logs日志

--- src/logs/logs_test.go
+++ src/logs/logs_test.go
@@ -3,8 +3,8 @@ package logs
 import "testing"
 
 func Test_initLogs_outPut_1(t *testing.T) {
-	InitLogger("./","app.log",2,true)
-	print("...,,,,,...")
+	InitLogger("./","app.log",1,true)
+	Print("...,,,,,...")
 	Debug("你好 wo china")
 	Info("............")
 	Warn("你好 my china")
