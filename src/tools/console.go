734aeef921f896e5d47d780a8fb4715d30ce5853
lnsx <zhang13652103006@163.com>
2016-12-27 14:59:19 +0800

    控制台

---
+++ src/tools/console.go
@@ -0,0 +1,67 @@
+package tools
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+	"loger"
+)
+
+type CommandHandler func(args []string) bool
+
+var (
+	command                              = make([]byte, 1024)
+	reader                               = bufio.NewReader(os.Stdin)
+	HandlerMap map[string]CommandHandler = make(map[string]CommandHandler, 20)
+)
+
+func StartConsole() {
+	go consoleroutine()
+}
+
+func StartConsoleWait() {
+	consoleroutine()
+}
+
+func consoleroutine() {
+	for {
+		command, _, _ = reader.ReadLine()
+		Args := strings.Split(string(command), " ")
+		cmdhandler, ok := HandlerMap[Args[0]]
+		if ok {
+			cmdhandler(Args)
+			continue
+		}
+		loger.Info(string(command),"..。。..",Args)
+		switch string(Args[0]) {
+		case "cpus":
+			fmt.Println(runtime.NumCPU(), " cpus and ", runtime.GOMAXPROCS(0), " in use")
+
+		case "routines":
+			fmt.Println("Current number of goroutines: ", runtime.NumGoroutine())
+
+		case "setcpus":
+			n, _ := strconv.Atoi(Args[1])
+			runtime.GOMAXPROCS(n)
+			fmt.Println(runtime.NumCPU(), " cpus and ", runtime.GOMAXPROCS(0), " in use")
+		case "startgc":
+			runtime.GC()
+			fmt.Println("gc finished")
+		default:
+			fmt.Println("Command error, try again.")
+		}
+	}
+}
+
+func HandleFunc(cmd string, mh CommandHandler) {
+	if HandlerMap == nil {
+		HandlerMap = make(map[string]CommandHandler, 20)
+	}
+
+	HandlerMap[cmd] = mh
+
+	return
+}
