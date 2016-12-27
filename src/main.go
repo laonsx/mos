0e37cfb5e0ae54f51c32554c34719c1bb74a6c9f
lnsx <zhang13652103006@163.com>
2016-12-27 13:54:20 +0800

    router

--- src/main.go
+++ src/main.go
@@ -1,10 +1,77 @@
 package main
 
 import (
-	"fmt"
 	_ "config"
+	"loger"
+	"net/http"
+	"routers"
 )
-func main() {
-	fmt.Println("hello world")
 
+/*type MyMux struct {
+
+}
+
+func (this MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		sayhelloName(w, r)
+		return
+	}
+	if r.URL.Path == "/ha" {
+		helloServer(w, r)
+		return
+	}
+
+	http.NotFound(w, r)
+	return
+}
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello myroute!")
+}
+
+func helloServer(w http.ResponseWriter,r *http.Request) {
+	auth:=r.Header.Get("Authorization")
+	if auth==""{
+		w.Header().Set("WWW-Authenticate", `Basic realm="Dotcoo User Login"`)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	loger.Debug(auth)
+	auths:=strings.SplitN(auth,"",2)
+	if len(auths)!=2{
+		loger.Error("error....")
+		return
+	}
+	authMethod:=auths[0]
+	authB64:=auths[1]
+	loger.Info(authMethod,authB64)
+	switch authMethod {
+	case "Basic":
+		authstr,err:=base64.StdEncoding.DecodeString(authB64)
+		if err!=nil{
+			loger.Error(err.Error())
+			io.WriteString(w,"unauthorized!\n")
+			return
+		}
+		loger.Info(string(authstr))
+		userPwd:=strings.SplitN(string(authstr),":",2)
+		if len(userPwd)!=2{
+			loger.Error(err.Error())
+			return
+		}
+		username:=userPwd[0]
+		password:=userPwd[1]
+		loger.Info(username,password)
+	default:
+		loger.Error("error")
+		return
+	}
+	io.WriteString(w,"hello world!\n")
+}*/
+
+func main() {
+	//mux := &MyMux{}
+	//http.ListenAndServe(":9090", mux)
+	loger.Info("start server on :9090")
+	http.ListenAndServe(":9090",routers.Routers)
 }
